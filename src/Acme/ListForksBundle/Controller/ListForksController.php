d5bc0a58a0ac6969fe27ae4b7a5df3a7d7ea048f
Benjamin <j8b7@ugrad.cs.ubc.ca>
2012-11-05 12:54:05 -0800

    changing the list controller to listforks

--- src/Acme/ListForksBundle/Controller/ListForksController.php
+++ src/Acme/ListForksBundle/Controller/ListForksController.php
@@ -12,10 +12,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ListForksController extends Controller
 {
-    /**
-     * @Route("/", name="_ListForks_home")
-     * @Template()
-     */
+    
+     @Route("/", name="_ListForks_home")
+     @Template()
     public function indexAction()
     {
         return array();
