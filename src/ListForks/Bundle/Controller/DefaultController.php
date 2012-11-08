f8721db8ef65bfda4f0c3ada9fc3902f514393e9
Raymond Chow <raymond_chow@shaw.ca>
2012-11-07 18:24:43 -0800

    Temporarily change default view to get rid of exception

--- src/ListForks/Bundle/Controller/DefaultController.php
+++ src/ListForks/Bundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ListForksBundle:Default:index.html.twig');
+        return new Response('Welcome to ListForks!');
     }
 }
