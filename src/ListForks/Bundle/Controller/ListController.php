5377378272bdef17710c9c9a026a9033c7b4d44f
Benjamin <j8b7@ugrad.cs.ubc.ca>
2012-11-10 17:14:05 -0800

    templating the views

--- src/ListForks/Bundle/Controller/ListController.php
+++ src/ListForks/Bundle/Controller/ListController.php
@@ -25,8 +25,19 @@ class ListController extends Controller
      * @Route("/create", name="_list_create")
      * @Template()
      */
-    public function createAction()
+    public function createAction( )
     {
+
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+
         return $this->render('ListForksBundle:List:create.html.twig');
     }
 
