1f74717a2c774a1c7de19edaf35c22dec5d5540c
Raymond Chow <raymond_chow@shaw.ca>
2012-11-12 22:30:10 -0800

    Add Account Profile View and Edit

--- src/ListForks/Bundle/Controller/AccountController.php
+++ src/ListForks/Bundle/Controller/AccountController.php
@@ -3,11 +3,18 @@
 namespace ListForks\Bundle\Controller;
 
 use ListForks\Bundle\Entity\Account;
+use ListForks\Bundle\Entity\Profile;
+
 use ListForks\Bundle\Form\Type\AccountType;
+use ListForks\Bundle\Form\Type\ProfileType;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+
+// imports the "@Secure" annotation
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 // these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -18,10 +25,24 @@ class AccountController extends Controller
 	/**
      * @Route("/", name="_account")
      * @Template()
+     * @Secure(roles="ROLE_USER")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('ListForksBundle:Account:index.html.twig');
+        $account = $this->get('security.context')->getToken()->getUser();
+
+        $profile = $this->getDoctrine()
+            ->getRepository('ListForksBundle:Profile')
+            ->findOneByAccount($account);
+
+        $form = $this->createForm(new ProfileType(), $profile);
+
+            if( $request->getMethod() == 'POST' )
+            {
+                $this->forward('ListForksBundle:Account:edit', array('form' => $form));
+            }
+
+        return $this->render('ListForksBundle:Account:index.html.twig', array('form' => $form->createView()));
     }
 
     /**
@@ -46,8 +67,12 @@ class AccountController extends Controller
                     $password = $encoder->encodePassword($account->getPassword(), $account->getSalt());
                     $account->setPassword($password);
 
+                    $profile = new Profile();
+                    $profile->setAccount($account);
+
                     $em = $this->getDoctrine()->getManager();
                     $em->persist($account);
+                    $em->persist($profile);
                     $em->flush();
 
                     return $this->redirect($this->generateUrl('_login'));
@@ -60,6 +85,7 @@ class AccountController extends Controller
     /**
      * @Route("/delete", name="_account_delete")
      * @Template()
+     * @Secure(roles="ROLE_USER")
      */
     public function deleteAction()
     {
@@ -80,26 +106,43 @@ class AccountController extends Controller
     /**
      * @Route("/edit", name="_account_edit")
      * @Template()
+     * @Secure(roles="ROLE_USER")
      */
-    public function editAction()
+    public function editAction(Request $request, Form $form)
     {
 
         $request = $this->get('request');
 
         // when edit form is submited via post
-        if ($request->getMethod() == 'POST') {
-      
+        if( $request->getMethod() == 'POST' )
+        {
+            $form->bindRequest($request);
 
+            if( $form->isValid() )
+            {
+                $profile = $form->getData();
 
+                $account = $profile->getAccount();
 
-            // Need to do something with the data here
+                $factory = $this->get('security.encoder_factory');
+                $encoder = $factory->getEncoder($account);
+
+                $account->setSalt( md5(uniqid(null, true)) );
+
+                $password = $encoder->encodePassword($account->getPassword(), $account->getSalt());
+                $account->setPassword($password);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('_account'));
+            }
         }
 
         // when requesting to get the edit from
         if ($request->getMethod() == 'GET') {
       
-
-            return $this->render('ListForksBundle:Account:edit.html.twig');
+            return $this->render('ListForksBundle:Account:index.html.twig', array('form' => $form->createView()));
 
             // Need to do something with the data here
         }
