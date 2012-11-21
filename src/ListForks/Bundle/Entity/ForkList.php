79717fced20c10444cff12b7546dd4ea07b0ac55
Raymond Chow <raymond_chow@shaw.ca>
2012-11-21 00:03:39 -0800

    Add createdAt and updatedAt attributes to ForkList Entity class and ListsController.

--- src/ListForks/Bundle/Entity/ForkList.php
+++ src/ListForks/Bundle/Entity/ForkList.php
@@ -22,6 +22,20 @@ class ForkList
     private $id;
 
     /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updatedAt", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
      * @var string $name
      *
      * @ORM\Column(name="name", type="string", length=255)
@@ -83,6 +97,52 @@ class ForkList
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return ForkList
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return ForkList
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
     }
 
     /**
@@ -307,4 +367,5 @@ class ForkList
     {
         return $this->subscriptions;
     }
+
 }
